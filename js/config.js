bd301da200614c46f80a5fe4ba671e8004c32503
Juanmtd <juanmtd02@gmail.com>
2026-05-02 21:41:36 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,22 +1,17 @@
 const CONFIG = {
-
-  API_URL: 'https://script.google.com/macros/s/AKfycbzdlr4hoL_E9nQkReBNHox-i05nblTq8MFzTDR7Rrdir8J3jd5ZCxEQHyIyVNHPJsZ8/exec',
-
-  API_TOKEN: 'w2_live_2026_JMT_wallet_api_8xQp91LmZ7Kf29Ab',
+  API_URL: "https://script.google.com/macros/s/AKfycbzdlr4hoL_E9nQkReBNHox-i05nblTq8MFzTDR7Rrdir8J3jd5ZCxEQHyIyVNHPJsZ8/exec",
+  TOKEN: "w2_live_2026_JMT_wallet_api_8xQp91LmZ7Kf29Ab",
 
   OWNERS: [
-
-    'Juan Miguel',
-    'Cristian',
-    'Daniel',
-    'Juan Pablo',
-    'Iker',
-    'Maryori',
-    'Lucy',
-    'TEDOS',
-    'Jesus Eduardo',
-    'Jesus Jonas'
-
+    "Juan Miguel",
+    "Cristian",
+    "Daniel",
+    "Juan Pablo",
+    "Iker",
+    "Maryori",
+    "Lucy",
+    "TEDOS",
+    "Jesus Eduardo",
+    "Jesus Jonas"
   ]
-
 };
