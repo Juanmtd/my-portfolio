ab5c15ca06a5760d80172ae3241cd3df4f81d965
Juanmtd <juanmtd02@gmail.com>
2026-05-02 21:12:15 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzdlr4hoL_E9nQkReBNHox-i05nblTq8MFzTDR7Rrdir8J3jd5ZCxEQHyIyVNHPJsZ8/exec',
-  API_TOKEN: 'w2_live_2026_JMT_wallet_api_8xQp91LmZ7Kf29Ab',
-  DEFAULT_VIEW: 'dashboard'
-};
