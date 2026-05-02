bb070342fd61ea7e27a31f0533938de49d2f5b70
Juanmtd <juanmtd02@gmail.com>
2026-05-02 21:27:57 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,22 @@
+const CONFIG = {
 
+  API_URL: 'TU_URL_APPS_SCRIPT',
+
+  API_TOKEN: 'TU_TOKEN',
+
+  OWNERS: [
+
+    'Juan Miguel',
+    'Cristian',
+    'Daniel',
+    'Juan Pablo',
+    'Iker',
+    'Maryori',
+    'Lucy',
+    'TEDOS',
+    'Jesus Eduardo',
+    'Jesus Jonas'
+
+  ]
+
+};
