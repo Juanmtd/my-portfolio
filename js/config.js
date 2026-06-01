348d245b5511ee5812c20c56928895170461ab8c
Juanmtd <juanmtd02@gmail.com>
2026-06-01 14:54:04 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,8 @@ const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbzdlr4hoL_E9nQkReBNHox-i05nblTq8MFzTDR7Rrdir8J3jd5ZCxEQHyIyVNHPJsZ8/exec",
   TOKEN: "w2_live_2026_JMT_wallet_api_8xQp91LmZ7Kf29Ab",
 
+  GOOGLE_CLIENT_ID: "442964568100-nc4i6rn97kprssbb9d737spi3fucs9ae.apps.googleusercontent.com",
+
   OWNERS: [
     "Juan Miguel",
     "Cristian",
